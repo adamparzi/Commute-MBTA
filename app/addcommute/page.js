3a56c3da899211f3df750452db68afa72d932635
Adam <adamparzi@gmail.com>
2024-08-16 18:17:57 -0400

    feat: added docker

---
+++ app/addcommute/page.js
@@ -0,0 +1,14 @@
+import Main from "@/components/Main";
+
+export const metadata = {
+    title: "Add A Commute",
+  };
+
+export default function AddCommutePage(){
+    return (
+        <Main>
+            Add a commute: Ask what line/bus, direction, and stop here.
+        </Main>
+
+    )
+}
---
+++ components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// main React file. 
+
+export default function Main(props) {
+    const { children } = props
+    return (
+        <main>
+            {children}
+        </main>
+  )
+}
