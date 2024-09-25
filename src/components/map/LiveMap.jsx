898981f77abeba6def64843738fa07311a1f2327
Adam S <adamparz@bu.edu>
2024-09-25 12:35:31 -0400

    disable leaflet-defaulticon-compatibility

--- src/components/map/LiveMap.jsx
+++ src/components/map/LiveMap.jsx
@@ -5,8 +5,8 @@ import { getCommutePrediction } from '@src/util/mainBoxLogic';
 import { useEffect, useRef } from 'react';
 
 import 'leaflet/dist/leaflet.css';
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css';
-import 'leaflet-defaulticon-compatibility';
+//import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css';
+//import 'leaflet-defaulticon-compatibility';
 
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
